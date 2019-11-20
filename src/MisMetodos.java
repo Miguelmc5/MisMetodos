367e493bdc7b2f2648365de21b114d8f8326c31c
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2019-11-20 10:47:55 +0100

    Update MisMetodos.java

--- src/MisMetodos.java
+++ src/MisMetodos.java
@@ -1,26 +1,16 @@
-
 import java.util.Scanner;
 
 public class MisMetodos {
 
-public float validaFloatPositivo(){
-    Scanner sc=new Scanner(System.in);
-    float valor;
-   
-    do{
-    valor=sc.nextFloat();
-    }
-    while(valor<=0);
-    return valor;
+    public static float validaFloatPositivo() {
+        Scanner sc = new Scanner(System.in);
+        float valor;
 
-}
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        do {
+            valor = sc.nextFloat();
+        } while (valor <= 0);
+        return valor;
+
+    }
+
 }
